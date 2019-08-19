1d8cd4be7d7126d220423609d8aba9c3c60450f5
yu <1134923982@qq.com>
2019-08-19 22:52:23 +0800

    init project

---
+++ src/main/java/com/exercises/exercise/config/DataSourceConfig.java
@@ -0,0 +1,19 @@
+package com.exercises.exercise.config;
+
+import com.alibaba.druid.stat.DruidDataSourceStatManagerMBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    @ConfigurationProperties("spring.datasource.one")
+    DataSource dsOne(){
+        {
+            return DruidDataSourceBuilder.create().build();
+//        return DruidDataSourceBuild.create().build();
+    }
+}
